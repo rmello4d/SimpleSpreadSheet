2c82cd7836cfe6d13d53c98ed6b208126d07c8c9
Ricardo <ricardo.mello@4d.com>
2013-10-21 21:10:09 +0200

    fixing error last commit, committed wrong files...

--- designer.js
+++ designer.js
@@ -1,6 +1,45 @@
 (function() {
-	
-    var SpreadSheet = Widget.SpreadSheet.inherit(WAF.require('waf-behavior/studio'));
+    var SimpleSpreadSheet = Widget.SimpleSpreadSheet.inherit(WAF.require('waf-behavior/studio'));
+
+	// adding attributes
+    SimpleSpreadSheet.addAttributes([{
+    	'name':'data-width',
+    	'description':'Width',
+    	'defaultValue': '600px'
+    },{
+    	'name':'data-height',
+    	'description':'Height',
+    	'defaultValue': '400px'
+    },{
+        name: 'data-binding-value',
+        description: 'Datasource',
+        typeValue: 'datasource'
+    },{
+		'name':'data-content',
+    	'description':'DS Content Attribute',
+    	'defaultValue': 'content'
+    }]);
+  
+    SimpleSpreadSheet.addEvent({ 'name': 'Change' });
+    SimpleSpreadSheet.addEvent({ 'name': 'Save' });
+       
+	// defining the action when user resize the widget in the Studio      
+    SimpleSpreadSheet.on('Resize', function(event) {
+    	this.studioCss('height', event.size.height);
+    	this.studioCss('width', event.size.width); 
+    	this.studioVal('data-width', event.size.width + 'px');
+    	this.studioVal('data-height', event.size.height + 'px');	 	
+	 	this.studioPanelRefresh();
+    });
+    
+
+    // defining the action when widget is displayed/updated in the Studio 
+	SimpleSpreadSheet.on('Display', function (attributes) {
+		//display width and height
+	 	this.studioCss('width', attributes['data-width']); 	
+	 	this.studioCss('height', attributes['data-height']); 		  
+	 });
+
 
 })();
 
--- widget.js
+++ widget.js
@@ -1,15 +1,50 @@
 (function() {
     var widget = WAF.require('waf-core/widget');
-    var SpreadSheet = widget.create('SpreadSheet');
+    var SimpleSpreadSheet = widget.create('SimpleSpreadSheet');
 
-	//in the Init phase of your widget, bind the code that is fired when the event occurs
-	 SpreadSheet.prototype.init = function () { 
+    var Event = WAF.require('waf-core/event');
+    Event.create('Save');
 
-		$('#' + this.id).wijspread({sheetCount:1}); // create wijspread control
-		var spread = $('#' + this.id).wijspread("spread"); // get instance of wijspread control
-		var sheet = spread.getActiveSheet();
+	SimpleSpreadSheet.prototype.init = function () { 			
+        // setup the html template
+        this.node.innerHTML = '<div></div><button>Save</button>';
+
+        // create the spreadsheet control
+        var spreadnode = $('>div', this.node);
+        if(spreadnode.wijspread) {
+            spreadnode.wijspread({sheetCount:1}); 
+
+            // save the spread object
+            this.spread = spreadnode.wijspread('spread'); 
+            this.spread.addSheet(0);
+
+            //click on save button 
+            $('>button', this.node).on ('click', function () {	    	   	
+                // fire an event telling the the 'value' property is updated (to synchronise the datasources)
+                this.fire(new Event.Change('value', { value: this.value() }));
+                this.fire(new Event.Save());
+            }.bind(this)); 
+        }
 	}
-
+		
+	// adding a property automatically makes the property bindable
+    SimpleSpreadSheet.addProperty('value', {
+        setter: function(v) {
+            console.log('set', v);
+            if(v) {
+                try {
+                    this.spread.fromJSON(JSON.parse(v));
+                    return;
+                } catch(e) {
+                }
+            }
+            console.log('clear');
+            this.spread.clearSheets();
+            this.spread.addSheet(0);
+        },
+        getter: function() {
+            console.log('get', JSON.stringify(this.spread));
+            return JSON.stringify(this.spread);
+        }
+    });
 })();
-
-// For more information, refer to http://doc.wakanda.org/Wakanda0.DevBranch/help/Title/en/page3871.html
